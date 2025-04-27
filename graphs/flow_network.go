74228e1b4d18102738dc5ddccf7e91f1b5e4b35d
d-zaytsev <dzaytsev308@gmail.com>
2025-04-27 16:59:37 +0300

    Safety flow network fixes

--- graphs/flow_network.go
+++ graphs/flow_network.go
@@ -18,12 +18,24 @@ func MakeFlowNetwork() *FlowNetwork {
 	}
 }
 
-func (g *FlowNetwork) AddEdge(vertex1, vertex2 FlowNetworkVertex, edge FlowNetworkEdge) (err error) {
+func (g *FlowNetwork) AddVertex(vertex FlowNetworkVertex) error {
+	_, exists := g.vertices[vertex]
+
+	if exists {
+		return fmt.Errorf("Can't add new vertex '%v', it is already exists.", vertex)
+	}
+
+	g.vertices[vertex] = make(map[FlowNetworkVertex]FlowNetworkEdge)
+
+	return nil
+}
+
+func (g *FlowNetwork) AddEdge(vertex1, vertex2 FlowNetworkVertex, edge FlowNetworkEdge) error {
 	_, exists1 := g.vertices[vertex1]
 	_, exists2 := g.vertices[vertex2]
 
 	if !exists1 || !exists2 {
-		return fmt.Errorf("Failed to add edge.")
+		return fmt.Errorf("Can't add edge: vertex %v or %v doesn't exist.", vertex1, vertex2)
 	}
 
 	g.vertices[vertex1][vertex2] = edge
@@ -31,12 +43,31 @@ func (g *FlowNetwork) AddEdge(vertex1, vertex2 FlowNetworkVertex, edge FlowNetwo
 	return nil
 }
 
-func (g *FlowNetwork) RemoveEdge(vertex1, vertex2 FlowNetworkVertex) {
+func (g *FlowNetwork) RemoveEdge(vertex1, vertex2 FlowNetworkVertex) error {
+	_, exists := g.vertices[vertex1][vertex2]
+
+	if !exists {
+		return fmt.Errorf("Can't remove edge (%v,%v). It doesn't exist.", vertex1, vertex2)
+	}
+
 	delete(g.vertices[vertex1], vertex2)
+
+	return nil
 }
 
-func (g *FlowNetwork) GetNeighbors(vertex FlowNetworkVertex) map[FlowNetworkVertex]FlowNetworkEdge {
-	return g.vertices[vertex]
+func (g *FlowNetwork) GetNeighbors(vertex FlowNetworkVertex) (map[FlowNetworkVertex]FlowNetworkEdge, error) {
+	neighbors, exists := g.vertices[vertex]
+
+	if !exists {
+		return nil, fmt.Errorf("Can't get neighbors of %v. It doesn't exist.", vertex)
+	}
+
+	copyMap := make(map[FlowNetworkVertex]FlowNetworkEdge)
+	for k, v := range neighbors {
+		copyMap[k] = v
+	}
+
+	return copyMap, nil
 }
 
 func (g *FlowNetwork) HasEdge(vertex1, vertex2 FlowNetworkVertex) bool {
