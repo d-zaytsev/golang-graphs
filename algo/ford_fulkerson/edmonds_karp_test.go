c47c244f4e53681057e0357baf1b1a4f8ec97347
d-zaytsev <dzaytsev308@gmail.com>
2025-05-03 13:34:27 +0300

    Simplify tests

--- algo/ford_fulkerson/edmonds_karp_test.go
+++ algo/ford_fulkerson/edmonds_karp_test.go
@@ -1,97 +1,35 @@
 package algo
 
 import (
-	g "dzaytsev/golang-graphs/graphs"
 	"testing"
 )
 
-func TestEdmondsAlgo1(t *testing.T) {
-	network := g.MakeFlowNetwork[float64]()
-
-	for i := 0; i <= 3; i++ {
-		network.AddVertex(g.FlowNetworkVertex(i))
+func TestEdmondsKarp(t *testing.T) {
+	tests := []struct {
+		name         string
+		builder      func() (*NetworkTaskData, error)
+		expectedFlow float64
+	}{
+		{"Test 1", buildTaskData1, 10},
+		{"Test 2", buildTaskData2, 10},
+		{"Test 3", buildTaskData3, 2000},
 	}
 
-	network.AddEdge(0, 1, g.FlowNetworkEdge[float64]{Capacity: 10})
-	network.AddEdge(0, 2, g.FlowNetworkEdge[float64]{Capacity: 5})
-	network.AddEdge(1, 2, g.FlowNetworkEdge[float64]{Capacity: 15})
-	network.AddEdge(2, 3, g.FlowNetworkEdge[float64]{Capacity: 10})
-	network.AddEdge(3, 1, g.FlowNetworkEdge[float64]{Capacity: 20})
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			testData, err := tt.builder()
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	test_data, _ := MakeNetworkTaskData(network, 0, 3)
+			res, err := testData.EdmondsKarp()
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	res, err := test_data.EdmondsKarp()
-
-	if err != nil {
-		t.Error(err)
-		return
-	}
-
-	if res != 10 {
-		t.Errorf("Incorrect result path: %v!", res)
-	}
-}
-
-func TestEdmondsAlgo2(t *testing.T) {
-	network := g.MakeFlowNetwork[float64]()
-
-	for i := 0; i <= 5; i++ {
-		network.AddVertex(g.FlowNetworkVertex(i))
-	}
-
-	network.AddEdge(0, 1, g.FlowNetworkEdge[float64]{Capacity: 4})
-	network.AddEdge(0, 2, g.FlowNetworkEdge[float64]{Capacity: 7})
-
-	network.AddEdge(1, 3, g.FlowNetworkEdge[float64]{Capacity: 4})
-	network.AddEdge(1, 4, g.FlowNetworkEdge[float64]{Capacity: 8})
-
-	network.AddEdge(2, 1, g.FlowNetworkEdge[float64]{Capacity: 4})
-	network.AddEdge(2, 4, g.FlowNetworkEdge[float64]{Capacity: 2})
-
-	network.AddEdge(3, 5, g.FlowNetworkEdge[float64]{Capacity: 12})
-
-	network.AddEdge(4, 3, g.FlowNetworkEdge[float64]{Capacity: 4})
-	network.AddEdge(4, 5, g.FlowNetworkEdge[float64]{Capacity: 5})
-
-	test_data, _ := MakeNetworkTaskData(network, 0, 5)
-
-	res, err := test_data.EdmondsKarp()
-
-	if err != nil {
-		t.Error(err)
-		return
-	}
-
-	if res != 10 {
-		t.Errorf("Incorrect result path: %v!", res)
-	}
-}
-
-func TestEdmondsAlgo3(t *testing.T) {
-	network := g.MakeFlowNetwork[float64]()
-
-	for i := 0; i <= 3; i++ {
-		network.AddVertex(g.FlowNetworkVertex(i))
-	}
-
-	network.AddEdge(0, 1, g.FlowNetworkEdge[float64]{Capacity: 1000})
-	network.AddEdge(0, 2, g.FlowNetworkEdge[float64]{Capacity: 1000})
-
-	network.AddEdge(1, 2, g.FlowNetworkEdge[float64]{Capacity: 1})
-	network.AddEdge(1, 3, g.FlowNetworkEdge[float64]{Capacity: 1000})
-
-	network.AddEdge(2, 3, g.FlowNetworkEdge[float64]{Capacity: 1000})
-
-	test_data, _ := MakeNetworkTaskData(network, 0, 3)
-
-	res, err := test_data.EdmondsKarp()
-
-	if err != nil {
-		t.Error(err)
-		return
-	}
-
-	if res != 2000 {
-		t.Errorf("Incorrect result path: %v!", res)
+			if res != tt.expectedFlow {
+				t.Errorf("Incorrect result path: got %v, want %v", res, tt.expectedFlow)
+			}
+		})
 	}
 }
---
+++ algo/ford_fulkerson/test_utils.go
@@ -0,0 +1,80 @@
+package algo
+
+import (
+	g "dzaytsev/golang-graphs/graphs"
+)
+
+type NetworkTaskBuilder struct {
+	network_builder func() *NetworkTaskData
+}
+
+func buildTaskData1() (*NetworkTaskData, error) {
+	network := g.MakeFlowNetwork[float64]()
+
+	for i := 0; i <= 3; i++ {
+		network.AddVertex(g.FlowNetworkVertex(i))
+	}
+
+	network.AddEdge(0, 1, g.FlowNetworkEdge[float64]{Capacity: 10})
+	network.AddEdge(0, 2, g.FlowNetworkEdge[float64]{Capacity: 5})
+	network.AddEdge(1, 2, g.FlowNetworkEdge[float64]{Capacity: 15})
+	network.AddEdge(2, 3, g.FlowNetworkEdge[float64]{Capacity: 10})
+	network.AddEdge(3, 1, g.FlowNetworkEdge[float64]{Capacity: 20})
+
+	test_data, err := MakeNetworkTaskData(network, 0, 3)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return test_data, nil
+
+}
+
+func buildTaskData2() (*NetworkTaskData, error) {
+	network := g.MakeFlowNetwork[float64]()
+
+	for i := 0; i <= 3; i++ {
+		network.AddVertex(g.FlowNetworkVertex(i))
+	}
+
+	network.AddEdge(0, 1, g.FlowNetworkEdge[float64]{Capacity: 10})
+	network.AddEdge(0, 2, g.FlowNetworkEdge[float64]{Capacity: 5})
+	network.AddEdge(1, 2, g.FlowNetworkEdge[float64]{Capacity: 15})
+	network.AddEdge(2, 3, g.FlowNetworkEdge[float64]{Capacity: 10})
+	network.AddEdge(3, 1, g.FlowNetworkEdge[float64]{Capacity: 20})
+
+	test_data, err := MakeNetworkTaskData(network, 0, 3)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return test_data, nil
+
+}
+
+func buildTaskData3() (*NetworkTaskData, error) {
+	network := g.MakeFlowNetwork[float64]()
+
+	for i := 0; i <= 3; i++ {
+		network.AddVertex(g.FlowNetworkVertex(i))
+	}
+
+	network.AddEdge(0, 1, g.FlowNetworkEdge[float64]{Capacity: 1000})
+	network.AddEdge(0, 2, g.FlowNetworkEdge[float64]{Capacity: 1000})
+
+	network.AddEdge(1, 2, g.FlowNetworkEdge[float64]{Capacity: 1})
+	network.AddEdge(1, 3, g.FlowNetworkEdge[float64]{Capacity: 1000})
+
+	network.AddEdge(2, 3, g.FlowNetworkEdge[float64]{Capacity: 1000})
+
+	test_data, err := MakeNetworkTaskData(network, 0, 3)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return test_data, nil
+
+}
